0710e7b102674a4cbe6beafcf6036e15a859b6e9
dev <dev@local>
2026-06-02 18:07:29 +0000

    fix: useEffect导入

--- src/app/project/[id]/script/page.tsx
+++ src/app/project/[id]/script/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import {LuWand, LuClock, LuImage, LuArrowRight, LuBookmarkPlus} from "react-icons/lu";
 import Link from "next/link";
